15afae99f307c69d523e43c872e1dde2d95e7bd5
CODEMCD <psbum1609@gmail.com>
2020-01-09 16:54:40 +0900

    init project

---
+++ src/main/java/com/codemcd/springbootvuejsweb/SpringBootVuejsWebApplication.java
@@ -0,0 +1,13 @@
+package com.codemcd.springbootvuejsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVuejsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVuejsWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codemcd/springbootvuejsweb/SpringBootVuejsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemcd.springbootvuejsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVuejsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
